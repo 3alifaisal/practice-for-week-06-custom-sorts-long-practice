7af730a384c33a94111b1d95f786390cf437d0e1
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2023-12-28 08:44:20 +0100

    finished project
    custom sorting is quite interesting need some practice to get better at using it

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,21 +1,42 @@
 function ageSort(users) {
-  // Your code here
+ return users.sort((a,b) => a.age - b.age)
 }
 
 function oddEvenSort(arr) {
-  // Your code here
+ return arr.sort((a,b) => {
+  if(a%2 ===0 && b%2 ===1) return 1;
+  if(a%2 === 1 && b%2 ===0) return -1;
+  return a-b;
+ })
 }
 
 function validAnagrams(s, t) {
-  // Your code here
+  return ( s.split("").sort().join("") === t.split("").sort().join(""))
 }
 
 function reverseBaseSort(arr) {
   // Your code here
+  return arr.sort((a,b) => {
+    if(Math.floor(Math.log10(a)+1) > Math.floor(Math.log10(b)+1)) return -1;
+    if (Math.floor(Math.log10(a) + 1) < Math.floor(Math.log10(b) + 1)) return 1;
+    return a-b;
+  })
 }
 
 function frequencySort(arr) {
-  // Your code here
+  let mapArr = new Map();
+  for(const num of arr){
+    mapArr.set(num, (mapArr.get(num) || 0) + 1);
+  }
+  return arr.sort((a,b) => {
+   let freqA = mapArr.get(a);
+   let freqB = mapArr.get(b);
+   if(freqA !== freqB){
+    return freqA - freqB;
+   }
+
+   return b-a;
+  })
 }
 
 module.exports = [
